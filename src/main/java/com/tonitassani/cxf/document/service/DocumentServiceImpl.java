9395a83c889126097fe20dca70defa151ef5b09b
unknown <atassani@BCN-50JWM12.usersad.everis.int>
2015-08-03 15:15:45 +0200

    Modified message

--- src/main/java/com/tonitassani/cxf/document/service/DocumentServiceImpl.java
+++ src/main/java/com/tonitassani/cxf/document/service/DocumentServiceImpl.java
@@ -11,7 +11,7 @@ public class DocumentServiceImpl implements DocumentService {
 	@Override
 	public DocumentOutput getDocumentAsJson(Long id, String version, String appId) {
 		Document document;
-		document = new Document(id.intValue(), "El document " + version + ", " + appId);
+		document = new Document(id.intValue(), "El document version=" + version + ", appId=" + appId);
 		
 		DocumentOutput output = new DocumentOutput();
 		output.setId(document.getId());
